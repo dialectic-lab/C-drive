d31e915827db1c0a72bb659d1d47b8c59d8bc6ed
Dialectic Lab <dialectic.data@gmail.com>
2025-01-03 04:26:02 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "C-Drive",
+    pageTitleSuffix: "Chandni's Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
